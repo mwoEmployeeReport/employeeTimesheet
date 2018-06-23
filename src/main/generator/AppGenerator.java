474cfec89fc71a37b74f247f48050dbede039819
student2 <student2@pcoip.ki.agh.edu.pl>
2018-06-23 15:40:06 +0200

    generate exel employee file

--- src/main/generator/AppGenerator.java
+++ src/main/generator/AppGenerator.java
@@ -1,12 +1,67 @@
 package main.generator;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Random;
+import java.util.Scanner;
+
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
+
 public class AppGenerator {
 
-    Workbook wb = new HSSFWorkbook();  // or new XSSFWorkbook();
-    Sheet sheet1 = wb.createSheet("new sheet");
-    Sheet sheet2 = wb.createSheet("second sheet");
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+
+        createExel();
+    }
+
+    public static void createExel() throws IOException {
+
+        Scanner read = new Scanner(System.in);
+        System.out.println("Podaj Liczbe pracownikow do wygenerowania");
+        int numberEmployee = 0;
+        numberEmployee = read.nextInt();
+
+        Workbook wb = new HSSFWorkbook();
+        for (int i = 1; i<=numberEmployee; i++ ) {
+            try (OutputStream fileOut = new FileOutputStream("Pracownik" + i + ".xls"))
+            {
+                Sheet sheet1 = wb.createSheet("Projekt" + i);
+                //Sheet sheet2 = wb.createSheet("Projekt2");
+                Row row = sheet1.createRow(0);
+                Cell cell0 = row.createCell(0);
+                cell0.setCellValue("Data");
+                Cell cell1 = row.createCell(1);
+                cell1.setCellValue("Zadanie");
+                Cell cell2 = row.createCell(2);
+                cell2.setCellValue("Czas [h]");
+                Row row1 = sheet1.createRow(1);
+
+                Cell cell10 = row1.createCell(0);
+                cell10.setCellValue("2018-05-01");
+
+                Cell cell11 = row1.createCell(1);
+                cell11.setCellValue("Zadanie1");
+
+                Random generator = new Random();
+                for (int h = 1; h < 10; h++) {
+                    int j = generator.nextInt(10) + 1;
+                    Row row2 = sheet1.createRow(h);
+                    Cell cell12 = row2.createCell(2);
+                    cell12.setCellValue(j);
+                    wb.write(fileOut);
+                }
+                fileOut.close();
+                wb.close();
+            }
+        }
+    }
 }
+
+
