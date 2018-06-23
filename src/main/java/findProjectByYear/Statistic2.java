1e34ba1c359586324fb79f5b2303960ab97d34b2
student7 <student7@pcoip.ki.agh.edu.pl>
2018-06-23 15:14:40 +0200

    added new branch

---
+++ src/main/java/findProjectByYear/Statistic2.java
@@ -0,0 +1,21 @@
+package findProjectByYear;
+
+import java.util.*; 
+
+ 
+
+public class Statistic2 
+{
+	int hours;
+	String projectName;
+
+	
+	public Statistic2 (int hours, String projectName)
+	{
+		 this.hours = hours; 
+         this.projectName = projectName;
+	}
+	
+}
+
+public class HashSet
